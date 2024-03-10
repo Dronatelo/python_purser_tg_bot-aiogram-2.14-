7ab08131f7b15e5c89e03dbee2b0f4ff43ac43a2
David <david1103tokarev2013@ya.ru>
2024-03-10 19:56:10 +0300

    car_purser(+proxy)

--- handlers/car_page_link.py
+++ handlers/car_page_link.py
@@ -6,6 +6,7 @@ from keyboards.main_kb import main_menu
 #@dp.message_handler(Text(equals = 'Сайт'))
 async def get_car_link(message: types.Message):
     await message.answer("https://www.mobile.de/ru",disable_web_page_preview=True, reply_markup=main_menu)
+    return
 
     
 def register_handlers_car_link(dp: Dispatcher):
--- handlers/youtube_page_link.py
+++ handlers/youtube_page_link.py
@@ -6,7 +6,7 @@ from keyboards.main_kb import main_menu
 #@dp.message_handler(Text(equals = 'YouTube'))
 async def get_youtube_link(message: types.Message):
     await message.answer("https://www.youtube.com/********",reply_markup=main_menu)
-
+    return
     
 def register_handlers_youtube_link(dp: Dispatcher):
     dp.register_message_handler(get_youtube_link, Text(equals="YouTube"),state=None)
